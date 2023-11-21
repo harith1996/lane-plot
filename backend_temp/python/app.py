8fbfd2cbb1ae4575e3b7081ece2e1d29b75c8329
Harith Rathish <harith1996@gmail.com>
2023-11-21 15:03:58 +0100

    temp: read wikipedia data intop dataframe

--- backend_temp/python/app.py
+++ backend_temp/python/app.py
@@ -4,9 +4,9 @@ from flask import Flask, redirect, url_for, request, jsonify
 from flask_cors import CORS
 from data_processing.dataService import DataService
 
-filename = "raw_data/2023-10.enwiki.2023-10.tsv"
+filename = "./backend_temp/raw_data/2023-10.enwiki.2023-10.tsv"
 
-#check file extension
+# check file extension
 sep = ","
 if filename.split(".")[-1] == "tsv":
     sep = "\t"
@@ -15,18 +15,26 @@ p = 1  # fraction of the data/
 # if random from [0,1] interval is greater than p the row will be skipped
 random.seed(4)
 
-df = pd.read_csv(
-    filename,
+columns = open(
+    "backend_temp/raw_data/2023-10.enwiki.2023-10_columns.txt", "r"
+).readlines()
+columns = list(line.split("\n")[0] for line in columns)
+
+reader = pd.read_csv(
+    filename,iterator=True,
     # header=0,
     skiprows=lambda i: i > 0 and random.random() > p,
     # skiprows = lambda i : i > 1000,
     on_bad_lines="skip",
-    sep = sep
+    sep=sep,
+    names=columns,
 )
+df = reader.get_chunk(500)
 app = Flask(__name__)
 CORS(app)
 
-ds = DataService(df, {"Time": "dateTime"})
+ds = DataService(df, {"event_timestamp": "dateTime"})
+
 
 @app.route("/")
 def hello_world():
@@ -49,6 +57,12 @@ def data_scales():
 def data_mins():
     return (df.min(axis=0, numeric_only=True)).to_json()
 
+
 @app.route(f"/preview")
 def data_preview():
     return df.head(100).to_json(orient="values")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
