b20adb7d938faa33c305825dd6f895937ed99f43
Harith Rathish <harith1996@gmail.com>
2023-11-22 18:50:10 +0100

    added d3-hexbin

--- src/components/Scatterplot.tsx
+++ src/components/Scatterplot.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useD3 } from "../hooks/useD3";
 import { Plot } from "../types/PlotsTypes";
 import * as d3 from "d3";
+import * as d3Hexbin from "d3-hexbin";
 type ScatterplotProps = {
 	plot: Plot;
 	selectionCallback: any;
@@ -17,22 +18,30 @@ export default function Scatterplot(props: ScatterplotProps) {
 			const width = 500;
 			const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
-			const x = d3
+			const xScale = d3
 				.scaleLinear()
-				.domain(plot.options.xDomain)
-				.rangeRound([margin.left, width - margin.right]);
+				.domain(
+					plot.options.xDomain ||
+						(d3.extent(data, (d) => d.x) as [number, number])
+				)
+				.rangeRound([margin.left, width - margin.right])
+				.clamp(true);
 
-			const y = d3
+			const yScale = d3
 				.scaleLinear()
-				.domain(plot.options.yDomain)
-				.rangeRound([height - margin.bottom, margin.top]);
+				.domain(
+					plot.options.yDomain ||
+						(d3.extent(data, (d) => d.y) as [number, number])
+				)
+				.rangeRound([height - margin.bottom, margin.top])
+				.clamp(true);
 
-			const xTicks: [number, number] = d3.extent(x.domain()) as [
+			const xTicks: [number, number] = d3.extent(xScale.domain()) as [
 				number,
 				number
 			];
 
-			const yTicks: [number, number] = d3.extent(y.domain()) as [
+			const yTicks: [number, number] = d3.extent(yScale.domain()) as [
 				number,
 				number
 			];
@@ -42,11 +51,11 @@ export default function Scatterplot(props: ScatterplotProps) {
 					.attr("transform", `translate(0,${height - margin.bottom})`)
 					.call(
 						d3
-							.axisBottom(x)
+							.axisBottom(xScale)
 							.tickValues(
 								d3
-									.ticks(...xTicks, width / 40)
-									.filter((v) => x(v) !== undefined)
+									.ticks(...xTicks, width / 80)
+									.filter((v) => xScale(v) !== undefined)
 							)
 							.tickSizeOuter(0)
 					);
@@ -54,11 +63,11 @@ export default function Scatterplot(props: ScatterplotProps) {
 			const yAxis = (g: any) =>
 				g.attr("transform", `translate(${margin.left},0)`).call(
 					d3
-						.axisLeft(y)
+						.axisLeft(yScale)
 						.tickValues(
 							d3
-								.ticks(...yTicks, height / 40)
-								.filter((v) => x(v) !== undefined)
+								.ticks(...yTicks, height / 80)
+								.filter((v) => xScale(v) !== undefined)
 						)
 						.tickSizeOuter(0)
 				);
@@ -67,16 +76,44 @@ export default function Scatterplot(props: ScatterplotProps) {
 			svg.select(".y-axis").call(yAxis);
 
 			// Append the dots.
+			const radius = 5;
+			const hexbin = d3Hexbin
+				.hexbin()
+				.x((d) => xScale(d[0]))
+				.y((d) => yScale(d[1]))
+				.radius((radius * width) / (height - 1))
+				.extent([
+					[margin.left, margin.top],
+					[width - margin.right, height - margin.bottom],
+				]);
+			let bins = hexbin(data.map((d) => [d.x, d.y]));
+			const colorScale = d3
+				.scaleSequentialSymlog(d3.interpolateBlues)
+				.domain([0, Math.max(...bins.map((b) => b.length))]);
 			const plotArea = svg.select(".plot-area");
+			plotArea
+				.attr("stroke", "#000")
+				.attr("stroke-opacity", 0.5)
+				.selectAll("path")
+				.data(bins)
+				.join("path")
+				.attr("d", hexbin.hexagon())
+				.attr("transform", (d) => `translate(${d.x},${d.y})`)
+				.attr("fill", (d) => colorScale(d.length));
+			/*************** BINNED SCATTERPLOT */
+
+			/*************** SCATTERPLOT
 			const dot = plotArea
 					.attr("stroke", "steelblue")
 					.attr("stroke-width", 0)
 				.selectAll("circle")
 				.data(data)
 				.join("circle")
-					.attr("transform", (d) => `translate(${x(d.x)},${y(d.y)})`)
+					.attr("transform", (d) => `translate(${xScale(d.x)},${yScale(d.y)})`)
 					.attr("r", 7)
 					.attr("opacity", 0.3);
+			****************/
+
 			// svg.call(
 			// 	d3.brush().on("start brush end", ({ selection }) => {
 			// 		let value: any[] = [];
