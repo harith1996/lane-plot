0cdbef3f3c3fe47f200f0e18ed3cb8f553219656
Harith Rathish <harith1996@gmail.com>
2023-11-28 12:11:55 +0100

    prevent /attributes from firing twice, rename labels

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import LaNePlots from "./components/LaNePlots";
 import { Plot } from "./types/PlotsTypes";
 import DataService from "./services/dataService";
 
-
+const ds = new DataService("http://localhost:5000");
 function App() {
 	const [activePlots, setActivePlots] = React.useState<Plot[]>([]);
-	const ds = new DataService("http://localhost:5000");
 	useEffect(() => {
 		ds.fetchData().then((data) => {
 			console.log(data);
@@ -22,8 +21,8 @@ function App() {
 			setActivePlots([
 				{
 					labels: {
-						xLabel: "diffNext_revision_bytes",
-						yLabel: "diffPrev_revision_bytes",
+						xLabel: "diffNext_revision_text_bytes",
+						yLabel: "diffPrev_revision_text_bytes",
 					},
 					options: {
 						xDomain: undefined, //compute domain from min-max
@@ -36,39 +35,41 @@ function App() {
 	}, []);
 	const onFilterChange = (filterChange: any) => {
 		//fetch new data
-		ds.fetchData(filterChange.sliceBy, filterChange.sliceByValue).then((data) => {
-			console.log(data);
-			//preprocess data before sending to plots
-			const plotData = ds.plotsifyData(
-				data,
-				"diffNext_revision_text_bytes",
-				"diffPrev_revision_text_bytes"
-			);
+		ds.fetchData(filterChange.sliceBy, filterChange.sliceByValue).then(
+			(data) => {
+				console.log(data);
+				//preprocess data before sending to plots
+				const plotData = ds.plotsifyData(
+					data,
+					"diffNext_revision_text_bytes",
+					"diffPrev_revision_text_bytes"
+				);
 
-			setActivePlots([
-				{
-					labels: {
-						xLabel: "diffNext_revision_bytes",
-						yLabel: "diffPrev_revision_bytes",
+				setActivePlots([
+					{
+						labels: {
+							xLabel: "diffNext_revision_text_bytes",
+							yLabel: "diffPrev_revision_text_bytes",
+						},
+						options: {
+							xDomain: undefined, //compute domain from min-max
+							yDomain: undefined,
+						},
+						data: plotData,
 					},
-					options: {
-						xDomain: undefined, //compute domain from min-max
-						yDomain: undefined,
-					},
-					data: plotData,
-				},
-			]);
-		});
+				]);
+			}
+		);
 	};
 	return (
-			<div className="App">
-				<div>
-					<Filters onFilterChange={onFilterChange}></Filters>
-				</div>
-				<div>
-					<LaNePlots plots={activePlots}></LaNePlots>
-				</div>
+		<div className="App">
+			<div>
+				<Filters onFilterChange={onFilterChange}></Filters>
 			</div>
+			<div>
+				<LaNePlots plots={activePlots}></LaNePlots>
+			</div>
+		</div>
 	);
 }
 
