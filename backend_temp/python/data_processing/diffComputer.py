b9ce16c0278d9352b1e744fa164103ad6fa9504d
Harith Rathish <harith1996@gmail.com>
2023-11-29 15:15:17 +0100

    diff computation: check which time comes later

--- backend_temp/python/data_processing/diffComputer.py
+++ backend_temp/python/data_processing/diffComputer.py
@@ -8,7 +8,11 @@ class DiffComputer:
         out = None
         match self.dataType:
             case "dateTime": 
-                out = float(pd.Timedelta(v1 - v2).seconds)
+                #check which date comes later
+                if(v1 > v2):
+                    out = float(pd.Timedelta(v1 - v2).seconds)
+                else:
+                    out = float(pd.Timedelta(v2 - v1).seconds)
             case _:
                 out = float(v1) - float(v2)
                 if (self.relative == True):
