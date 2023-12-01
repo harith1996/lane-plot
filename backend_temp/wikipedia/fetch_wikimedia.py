8db514b76011611c91dd46ca8c24c44a37b2e085
Atamarado <52821119+Atamarado@users.noreply.github.com>
2023-12-01 16:23:27 +0100

    Data revision list processed and converted to JS

--- backend_temp/wikipedia/fetch_wikimedia.py
+++ backend_temp/wikipedia/fetch_wikimedia.py
@@ -2,29 +2,35 @@ import time
 import requests
 import pickle
 
-if __name__=="__main__":
-    article_name = "Earth"
-    history_url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/"+article_name+"/history"
+if __name__ == "__main__":
+    # article_name = "Earth"
+    # history_url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/"+article_name+"/history"
 
-    revisions = []
+    article_ids = [47758247, 33782488, 28922023, 31574141]
+    article_names = ["Murray_Art_Museum_Albury", "Lammers", "Peugeot_EX1_Concept", "Floyd_Bedbury"]
 
-    start_time = time.time()
-    try:
-        while True:
-            if len(revisions)%100 == 0:
-                print("Iteration", str(len(revisions)//20))
-            response = requests.get(history_url).json()
+    for article_name in article_names:
+        history_url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/" + article_name + "/history"
 
-            revisions += response["revisions"]
+        revisions = []
 
-            if "older" in response.keys():
-                history_url = response["older"]
-            else:
-                break
-    except Exception as error:
-        print("Exception:", error)
-    end_time = time.time()
-    print("Fetching of",str(len(revisions)),"revisions took", end_time-start_time, "seconds")
+        start_time = time.time()
+        try:
+            while True:
+                if len(revisions)%100 == 0:
+                    print("Iteration", str(len(revisions)//20))
+                response = requests.get(history_url).json()
 
-    with open('wiki_raw/'+article_name+'.pkl', 'wb') as f:
-        pickle.dump(revisions, f)
+                revisions += response["revisions"]
+
+                if "older" in response.keys():
+                    history_url = response["older"]
+                else:
+                    break
+        except Exception as error:
+            print("Exception:", error)
+        end_time = time.time()
+        print("Fetching of", str(len(revisions)),"revisions took", end_time-start_time, "seconds")
+
+        with open('wiki_raw/'+article_name+'.pkl', 'wb') as f:
+            pickle.dump(revisions, f)
---
+++ backend_temp/wikipedia/process_revisions.py
@@ -0,0 +1,82 @@
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__=="__main__":
+    # article_name = "Earth"
+    article_names = ["Earth", "Murray_Art_Museum_Albury", "Lammers", "Peugeot_EX1_Concept", "Floyd_Bedbury"]
+
+    for article_name in article_names:
+        file_path = "wiki_raw/"+article_name+".pkl"
+        with open(file_path, "rb") as f:
+            raw_data = pickle.load(f)
+
+        df = pd.DataFrame.from_dict(raw_data)
+
+        user_df = df['user'].apply(pd.Series)
+        user_df.rename({"id": "user_id", "name": "username"}, axis=1, inplace=True)
+        df = pd.concat([df, user_df], axis=1).drop('user', axis=1)
+
+        # Convert to timestamp time
+        df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+        df.drop("delta", axis=1, inplace=True)
+
+        # Count nans in user_id (anonymous contributions)
+        print("There are", str(df['user_id'].isna().sum()), "anonymous revisions out of", str(len(df)))
+
+        username_counts = df["username"].value_counts()
+        # plt.hist(username_counts, bins=max(username_counts))  # //5)
+        # plt.title(article_name)
+        # plt.show()
+
+        # plt.hist(username_counts, bins=max(username_counts)//5)
+        # plt.ylim(0, 50)
+        # plt.show()
+        #
+        # plt.hist(username_counts, bins=max(username_counts)//5)
+        # plt.ylim(0, 10)
+        # plt.show()
+        #
+        # plt.hist(username_counts[username_counts <= 200], bins=max(username_counts)//2)
+        # plt.ylim(0, 10)
+        # plt.show()
+
+        # Calculate time dilation
+        df.sort_values("timestamp", inplace=True)
+
+        # df.plot.line("timestamp", "size")
+        # # plt.ylim(0, 2.5*10**5)
+        # plt.title(article_name)
+        # plt.show()
+
+        # Get LaNe from timestamp
+        df["last_time"] = df["timestamp"].diff()
+        df["last_time"].fillna(-1)
+
+        df["next_time"] = df["timestamp"].shift(-1) - df["timestamp"]
+        df["next_time"].fillna(-1)
+
+        # df.plot.scatter("last_time", "next_time", alpha=0.3)
+        # plt.title(article_name)
+        # plt.show()
+
+        # Get LaNe from size
+        df.sort_values("size", inplace=True)
+
+        df["last_size"] = df["size"].diff()
+        df["last_size"].fillna(-1)
+
+        df["next_size"] = df["size"].shift(-1) - df["size"]
+        df["next_size"].fillna(-1)
+
+        df.plot.scatter("last_size", "next_size", alpha=0.3)
+        # plt.title(article_name)
+        # plt.xlim(0, 2500)
+        # plt.ylim(0, 2500)º
+        # plt.show()
+
+        df.sort_values("timestamp", inplace=True)
+
+        df.set_index("id", drop=False, inplace=True)
+        df.to_json("wiki_json/"+article_name+".json", orient="records")
